5342a84fc85201bcd74bd798bdc74217f7f5a194
EkateriniTri <136068628+EkateriniTri@users.noreply.github.com>
2024-03-07 17:15:10 +0100

    Added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="Search"
-              placeholder="Enter a city..."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Vienna</h1>
-      <ul>
-        <li>Wednesday 09:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
-                alt="Mostly Cloudy"
-                className="float-left"
-              />
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+    });
 
-              <span className="temperature">9</span>
-              <span className="unit">°C</span>
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="Search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 0% </li>
-            <li>Humidity: 70% </li>
-            <li>Wind: 16 km/h </li>
-          </ul>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Wednesday 09:00</li>
+          <li>{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <div className="float-left">
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png
+                  "
+                  alt="Mostly Cloudy"
+                  className="float-left"
+                />
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
+                <span className="unit">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 0% </li>
+              <li>Humidity: {weatherData.humidity} % </li>
+              <li>Wind:{weatherData.wind} km/h </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "6c4a1fa1oe455fb969c4b3876f90t341";
+    let city = "Vienna";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Vienna&key=6c4a1fa1oe455fb969c4b3876f90t341&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Your city is loading...";
+  }
 }
